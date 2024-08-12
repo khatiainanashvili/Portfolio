61f9fe5e704948a0c4b948ec072e2f2b38d636a6
khatiainanashvili <118260235+khatiainanashvili@users.noreply.github.com>
2024-08-12 16:47:36 +0400

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,10 +131,4 @@ MEDIA_ROOT = BASE_DIR / 'static/files'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'khatia.inanashvili.2@gmail.com'
-EMAIL_HOST_PASSWORD = 'KhatiaInana$hvili123'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+
